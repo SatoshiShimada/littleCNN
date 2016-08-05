9b9de90cc59d69df56e41dea067a854507892150
SatoshiShimada <mylinux1204@gmail.com>
2016-08-05 15:18:17 +0900

    Edit xml load program

--- xml/logic_xml.cpp
+++ xml/logic_xml.cpp
@@ -1,4 +1,5 @@
 
+#include <cstdio>
 #include <iostream>
 #include <string>
 #include <boost/property_tree/ptree.hpp>
@@ -8,8 +9,10 @@
 
 int main(int argc, char **argv)
 {
+	char name[100];
+
 	boost::property_tree::ptree pt;
-	read_xml("net.xml", pt);
+	read_xml("logic.xml", pt);
 
 	if(boost::optional<std::string> str = pt.get_optional<std::string>("littleCNN.network.name")) {
 		std::cout << str.get() << std::endl;
@@ -17,9 +20,17 @@ int main(int argc, char **argv)
 		std::cout << "root.str is nothing" << std::endl;
 	}
 
-	BOOST_FOREACH(const boost::property_tree::ptree::value_type& child, pt.get_child("littleCNN.network.layer1")) {
-		const std::string value = boost::lexical_cast<std::string>(child.second.data());
-		std::cout << value << std::endl;
+	for(int i = 0; ; i++) {
+		try {
+			sprintf(name, "littleCNN.network.layer%d", i + 1);
+			printf("%s\n", name);
+			BOOST_FOREACH(const boost::property_tree::ptree::value_type& child, pt.get_child(name)) {
+				const std::string value = boost::lexical_cast<std::string>(child.second.data());
+				std::cout << value << std::endl;
+			}
+		} catch(...) {
+			break;
+		}
 	}
 	return 0;
 }
