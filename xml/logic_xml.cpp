b1fe5cc3aa5a1866496f2bf004a687ca18b24f10
satoshi <mylinux1204@gmail.com>
2016-11-17 03:30:26 +0900

    Improve xml sample code

--- xml/logic_xml.cpp
+++ xml/logic_xml.cpp
@@ -7,12 +7,24 @@
 #include <boost/foreach.hpp>
 #include <boost/lexical_cast.hpp>
 
-int main(int argc, char **argv)
+int append_parameter(std::string first, std::string second)
+{
+	/* TODO: impliment to class (layer parameter) */
+	if(first == "type") {
+	} else if(first == "inputNum") {
+	} else if(first == "outputNum") {
+	} else if(first == "activation") {
+	} else if(first == "learningRate") {
+	}
+	return 0;
+}
+
+int load_network_parameter_from_xml(std::string filename)
 {
     char name[100];
 
     boost::property_tree::ptree pt;
-    read_xml("logic.xml", pt);
+    read_xml(filename, pt);
 
     if(boost::optional<std::string> str = pt.get_optional<std::string>("littleCNN.network.name")) {
         std::cout << str.get() << std::endl;
@@ -29,11 +41,18 @@ int main(int argc, char **argv)
                 const std::string first = boost::lexical_cast<std::string>(child.first.data());
                 const std::string second = boost::lexical_cast<std::string>(child.second.data());
                 std::cout << first << ": " << second << std::endl;
+				append_parameter(first, second);
             }
         } catch(...) {
             break;
         }
     }
     return 0;
+}
+
+int main(int argc, char **argv)
+{
+    load_network_parameter_from_xml("logic.xml");
+	return 0;
 }
 
