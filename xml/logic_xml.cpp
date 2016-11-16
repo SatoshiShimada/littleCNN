d4841be74c85b366bf15381598aafe6ac093832b
satoshi <mylinux1204@gmail.com>
2016-11-17 03:05:31 +0900

    Add sample code for decode xml with boost library

--- xml/logic_xml.cpp
+++ xml/logic_xml.cpp
@@ -9,29 +9,31 @@
 
 int main(int argc, char **argv)
 {
-	char name[100];
+    char name[100];
 
-	boost::property_tree::ptree pt;
-	read_xml("logic.xml", pt);
+    boost::property_tree::ptree pt;
+    read_xml("logic.xml", pt);
 
-	if(boost::optional<std::string> str = pt.get_optional<std::string>("littleCNN.network.name")) {
-		std::cout << str.get() << std::endl;
-} else {
-		std::cout << "root.str is nothing" << std::endl;
-	}
+    if(boost::optional<std::string> str = pt.get_optional<std::string>("littleCNN.network.name")) {
+        std::cout << str.get() << std::endl;
+    } else {
+        std::cout << "root.str is nothing" << std::endl;
+    }
 
-	for(int i = 0; ; i++) {
-		try {
-			sprintf(name, "littleCNN.network.layer%d", i + 1);
-			printf("%s\n", name);
-			BOOST_FOREACH(const boost::property_tree::ptree::value_type& child, pt.get_child(name)) {
-				const std::string value = boost::lexical_cast<std::string>(child.second.data());
-				std::cout << value << std::endl;
-			}
-		} catch(...) {
-			break;
-		}
-	}
-	return 0;
+    /* decode xml */
+    for(int i = 0; ; i++) {
+        try {
+            sprintf(name, "littleCNN.network.layer%d", i + 1);
+            printf("%s\n", name);
+            BOOST_FOREACH(const boost::property_tree::ptree::value_type& child, pt.get_child(name)) {
+                const std::string first = boost::lexical_cast<std::string>(child.first.data());
+                const std::string second = boost::lexical_cast<std::string>(child.second.data());
+                std::cout << first << ": " << second << std::endl;
+            }
+        } catch(...) {
+            break;
+        }
+    }
+    return 0;
 }
 
